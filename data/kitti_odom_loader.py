6236031efe61c850a9876bb9b92ba70782d280d4
Lucas Sloan <lucas.sloan@gmail.com>
2019-09-26 07:56:30 -0700

    Get the data prep pipeline working.
    A bunch of scipy.misc functions were deprecated.

--- data/kitti_odom_loader.py
+++ data/kitti_odom_loader.py
@@ -1,7 +1,8 @@
 from __future__ import division
 import numpy as np
 from path import Path
-import scipy.misc
+import imageio
+from PIL import Image
 from collections import Counter
 import os
 
@@ -51,10 +52,10 @@ class KittiOdomLoader(object):
         img_file = scene_data['dir']/'image_{}'.format(scene_data['cid'])/scene_data['frame_id'][tgt_idx]+'.png'
         if not img_file.isfile():
             return None
-        img = scipy.misc.imread(img_file)
+        img = imageio.imread(img_file)
         zoom_y = self.img_height/img.shape[0]
         zoom_x = self.img_width/img.shape[1]
-        img = scipy.misc.imresize(img, (self.img_height, self.img_width))
+        img = np.array(Image.fromarray(img).resize((self.img_width, self.img_height)))
         return img, zoom_x, zoom_y
 
     def read_calib_file(self, cid, filepath, zoom_x, zoom_y):
